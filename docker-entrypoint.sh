eb5750582f10199f89cf2c1d640ce2107290db7c
Samuel Li <samli88@users.noreply.github.com>
2019-09-29 22:10:36 -0300

    Fix Docker image, user / entrypoint

---
+++ docker-entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e
+
+if [ $(echo "$1" | cut -c1) = "-" ]; then
+  echo "$0: assuming arguments for dcrwallet"
+
+  set -- dcrd "$@"
+fi
+
+if [ $(echo "$1" | cut -c1) = "-" ] || [ "$1" = "dcrwallet" ]; then
+  mkdir -p "$DCRWALLET_DATA"
+  chmod 0755 "$DCRWALLET_DATA"
+  chown -R decred "$DCRWALLET_DATA"
+
+  echo "$0: setting appdata directory to $DCRWALLET_DATA"
+
+  set -- "$@" --appdata="$DCRWALLET_DATA"
+fi
+
+if [ "$1" = "dcrwallet" ]; then
+  echo
+  exec su-exec decred "$@"
+fi
+
+echo
+exec "$@"
